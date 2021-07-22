5fe8b31e1830c31dbe54e16c97175b2522713182
ahmed mostafa <57231433+AhmedMoostafa@users.noreply.github.com>
2021-07-22 14:54:04 +0200

    Update start.js

--- test/start.js
+++ test/start.js
@@ -1,7 +1,2 @@
 const expect = require("chai").expect;
 const { saveStaions } = require("../controllers/saveData");
-
-it("should throw error if watherID is null", function () {
-  const watherID = null;
-  expect(saveStaions.bind(this, watherID)).to.throw("error ocurred");
-});
