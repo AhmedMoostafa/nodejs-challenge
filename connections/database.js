1927efc8810374114f0a02421bd8146095ec000f
AhmedMoostafa <ahmed.moahmed442@gmail.com>
2021-07-22 10:57:25 +0200

    Init commit

--- connections/database.js
+++ connections/database.js
@@ -1,6 +1,4 @@
-const connectionString =
-  "postgres://dxvkftauejnauw:7e2f77a6559de406b0099fc9923fab2487782bace6dbce61ead6f0f7760a557c@ec2-44-194-145-230.compute-1.amazonaws.com:5432/d4df1tkng8t19d";
-
+const connectionString = process.env.pg_URL;
 const { Pool } = require("pg");
 
 const pool = new Pool({
